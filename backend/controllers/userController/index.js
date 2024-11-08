431d5a0afd3094f58b466ec1dc028c7ed8335b9b
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-11-08 18:00:27 +0100

    import delete user controller

--- backend/controllers/userController/index.js
+++ backend/controllers/userController/index.js
@@ -2,5 +2,6 @@ import { createUser } from './createUser.js';
 import { login } from './login.js';
 import { logout } from './logout.js';
 import { updateUser } from './updateUser.js';
+import { deleteUser } from './deleteUser.js';
 
-export { createUser, login, logout, updateUser };
+export { createUser, login, logout, updateUser, deleteUser };
