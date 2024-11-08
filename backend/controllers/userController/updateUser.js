679b4e22a8366d9df6309f684a6ca8c25424b62c
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-11-08 17:47:25 +0100

    add condition if the user want to update is password, if so the new password is hashed

--- backend/controllers/userController/updateUser.js
+++ backend/controllers/userController/updateUser.js
@@ -4,6 +4,12 @@ const prisma = new PrismaClient();
 
 export const updateUser = async (req, res) => {
   try {
+    if (req.body.password) {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(req.body.password, salt);
+      req.body.password = hashedPassword;
+    }
+
     const user = await prisma.user.update({
       where: {
         id: req.params.userID,
