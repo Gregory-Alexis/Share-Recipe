e462c2d159dbc5cb1688fb00910421b896697648
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-11-08 18:00:41 +0100

    add the delete route

--- backend/routes/user_routes.js
+++ backend/routes/user_routes.js
@@ -1,5 +1,11 @@
 import express from 'express';
-import { createUser, updateUser, login, logout } from '../controllers/userController/index.js';
+import {
+  createUser,
+  updateUser,
+  deleteUser,
+  login,
+  logout,
+} from '../controllers/userController/index.js';
 
 const router = express.Router();
 
@@ -7,5 +13,6 @@ router.post('/signup', createUser);
 router.post('/login', login);
 router.post('/logout', logout);
 router.put('/update/:userID', updateUser);
+router.delete('/delete/:userID', deleteUser);
 
 export default router;
