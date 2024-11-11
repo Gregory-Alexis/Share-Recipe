081aacdb23098a347139c7ea1d4b1bcadfc87946
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-11-11 13:43:58 +0100

    user route created and exported

--- backend/routes/user_routes.js
+++ backend/routes/user_routes.js
@@ -1,18 +1,12 @@
 import express from 'express';
-import {
-  createUser,
-  updateUser,
-  deleteUser,
-  login,
-  logout,
-} from '../controllers/userController/index.js';
+import { createUser, updateUser, deleteUser, login, logout } from '../controllers/user/index.js';
 
-const router = express.Router();
+const userRouter = express.Router();
 
-router.post('/signup', createUser);
-router.post('/login', login);
-router.post('/logout', logout);
-router.put('/update/:userID', updateUser);
-router.delete('/delete/:userID', deleteUser);
+userRouter.post('/signup', createUser);
+userRouter.post('/login', login);
+userRouter.post('/logout', logout);
+userRouter.put('/update/:userID', updateUser);
+userRouter.delete('/delete/:userID', deleteUser);
 
-export default router;
+export default userRouter;
