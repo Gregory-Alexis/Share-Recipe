5785eb74a17ec83d60c10f9ee4a5eed875e825c1
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-11-11 13:39:40 +0100

    CRUD for the user

---
+++ backend/controllers/user/deleteUser.js
@@ -0,0 +1,17 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export const deleteUser = async (req, res) => {
+  try {
+    await prisma.user.delete({
+      where: {
+        id: req.params.userID,
+      },
+    });
+
+    res.status(200).json({ message: 'User deleted' });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
