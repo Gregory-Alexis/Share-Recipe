1166e4e384217f42e9b2a62facd5247e78eac3c1
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-11-11 13:39:57 +0100

    import all the controller in one file

---
+++ backend/controllers/user/index.js
@@ -0,0 +1,7 @@
+import { createUser } from './createUser.js';
+import { login } from './login.js';
+import { logout } from './logout.js';
+import { updateUser } from './updateUser.js';
+import { deleteUser } from './deleteUser.js';
+
+export { createUser, login, logout, updateUser, deleteUser };
