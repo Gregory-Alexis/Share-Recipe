7ed7b9ff38ecd336aaa512b1ca5e82072214bb18
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-11-11 13:38:43 +0100

    create the recipe controller

---
+++ backend/controllers/recipe/createRecipe.js
@@ -0,0 +1,29 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export const createRecipe = async (req, res) => {
+  try {
+    const recipe = await prisma.recipe.create({
+      data: {
+        title: req.body.title,
+        content: req.body.content,
+        image_url: req.body.image_url,
+        user: {
+          connect: {
+            id: req.body.userID,
+          },
+        },
+        ingredient: {
+          connect: {
+            id: req.body.ingredientId,
+          },
+        },
+      },
+    });
+
+    res.json({ message: 'Recipe created', recipe });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
