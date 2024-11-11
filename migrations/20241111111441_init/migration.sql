6c528943411953a8bb0028c70cf8fd9a25bbf280
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-11-11 13:55:56 +0100

    set the default schema prisma location due to an error in it's location

--- migrations/20241111111441_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Recipe" DROP CONSTRAINT "Recipe_ingredientId_fkey";
-
--- AlterTable
-ALTER TABLE "Recipe" ALTER COLUMN "ingredientId" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "Recipe" ADD CONSTRAINT "Recipe_ingredientId_fkey" FOREIGN KEY ("ingredientId") REFERENCES "Ingredient"("id") ON DELETE SET NULL ON UPDATE CASCADE;
