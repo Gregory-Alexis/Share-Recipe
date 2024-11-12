99cea09248f7518b02adc07c1bb3dde530803b82
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-11-12 17:41:58 +0100

    import ingredient router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,11 +3,13 @@ import express from 'express';
 import recipeRouter from './recipe_routes.js';
 import userRouter from './user_routes.js';
 import reviewRouter from './review_routes.js';
+import ingredientRouter from './ingredient_routes.js';
 
 const router = express.Router();
 
 router.use('/', recipeRouter);
 router.use('/', userRouter);
 router.use('/', reviewRouter);
+router.use('/', ingredientRouter);
 
 export default router;
