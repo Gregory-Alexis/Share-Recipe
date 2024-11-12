2f80229421cfa3d85d6518cf2be518de5d4bd22d
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-11-12 17:43:35 +0100

    add route for the recipes get method

--- backend/routes/recipe_routes.js
+++ backend/routes/recipe_routes.js
@@ -4,9 +4,12 @@ import { updateRecipe } from '../controllers/recipe/updateRecipe.js';
 import { deleteRecipe } from '../controllers/recipe/deleteRecipe.js';
 import { validateSchema } from '../middleware/zod.js';
 import { CreateRecipeSchema, UpdateRecipeSchema } from '../schema/recipe.js';
+import { getAllRecipe, getOneRecipe } from '../controllers/recipe/getRecipes.js';
 
 const recipeRouter = express.Router();
 
+recipeRouter.get('/recipes', getAllRecipe);
+recipeRouter.get('/recipe/:recipeID', getOneRecipe);
 recipeRouter.post('/recipe', validateSchema(CreateRecipeSchema), createRecipe);
 recipeRouter.put('/update/recipe/:recipeID', validateSchema(UpdateRecipeSchema), updateRecipe);
 recipeRouter.delete('/delete/recipe/:recipeID', deleteRecipe);
