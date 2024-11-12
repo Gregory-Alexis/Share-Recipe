5d1621c311bdd6e224caa7a227ed7cf81e99a6b3
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-11-12 17:41:44 +0100

    controller to get all the recipes or a recipe by it's id

---
+++ backend/controllers/recipe/getRecipes.js
@@ -0,0 +1,39 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export const getAllRecipe = async (req, res) => {
+  try {
+    const recipes = await prisma.recipe.findMany({
+      where: {
+        id: req.body.recipeID,
+      },
+      include: {
+        review: true,
+        ingredients: true,
+      },
+    });
+
+    res.status(200).json({ recipes });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+export const getOneRecipe = async (req, res) => {
+  try {
+    const recipe = await prisma.recipe.findUnique({
+      where: {
+        id: req.params.recipeID,
+      },
+      include: {
+        review: true,
+        ingredients: true,
+      },
+    });
+
+    res.status(200).json({ recipe });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
