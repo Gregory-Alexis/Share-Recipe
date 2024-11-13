5cda93649c4fca921c6ec6e940101ad7905e0fd3
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-11-13 15:58:26 +0100

    add delete method to the route

--- backend/routes/review_routes.js
+++ backend/routes/review_routes.js
@@ -1,6 +1,8 @@
 import express from 'express';
+
 import { createReview } from '../controllers/review/createReview.js';
 import { updateReview } from '../controllers/review/updateReview.js';
+import { deleteReview } from '../controllers/review/deleteReview.js';
 import { validateSchema } from '../middleware/zod.js';
 import { CreateReviewSchema, UpdateReviewSchema } from '../schema/review.js';
 
@@ -8,5 +10,6 @@ const reviewRouter = express.Router();
 
 reviewRouter.post('/review', validateSchema(CreateReviewSchema), createReview);
 reviewRouter.put('/update/review/:reviewID', validateSchema(UpdateReviewSchema), updateReview);
+reviewRouter.delete('/delete/review/:reviewID', deleteReview);
 
 export default reviewRouter;
