a07491e05d251d29d08e5c45c0492218d5d4cc39
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-11-13 21:07:37 +0100

    correct c/c typo

--- backend/schema/review.js
+++ backend/schema/review.js
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 export const CreateReviewSchema = z.object({
   content: z.string(),
-  rating: z.number().min(3, 'The title must contain at least 3 characters '),
+  rating: z.number(),
 });
 
 export const UpdateReviewSchema = z.object({
