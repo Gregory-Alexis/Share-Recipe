7cb4a379c3b3e99b3a1f6479fed282b5bd328923
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-11-13 10:32:41 +0100

    import favorite recipe router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,6 +4,7 @@ import recipeRouter from './recipe_routes.js';
 import userRouter from './user_routes.js';
 import reviewRouter from './review_routes.js';
 import ingredientRouter from './ingredient_routes.js';
+import favoriteRecipeRouter from './favorite_recipe_routes.js';
 
 const router = express.Router();
 
@@ -11,5 +12,6 @@ router.use('/', recipeRouter);
 router.use('/', userRouter);
 router.use('/', reviewRouter);
 router.use('/', ingredientRouter);
+router.use('/', favoriteRecipeRouter);
 
 export default router;
