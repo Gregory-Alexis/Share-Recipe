d923bbae22a710bbe5e2bd1f08ebbf5e51e7eee8
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-11-13 15:52:19 +0100

    add conditions if the recipes doesn't exist

--- backend/controllers/recipe/getRecipes.js
+++ backend/controllers/recipe/getRecipes.js
@@ -14,6 +14,10 @@ export const getAllRecipe = async (req, res) => {
       },
     });
 
+    if (!recipes) {
+      return res.status(404).json({ message: 'Recipes not found' });
+    }
+
     res.status(200).json({ recipes });
   } catch (error) {
     res.status(400).json({ message: error.message });
@@ -31,6 +35,10 @@ export const getOneRecipe = async (req, res) => {
         ingredients: true,
       },
     });
+
+    if (!recipe) {
+      return res.status(404).json({ message: 'Recipes not found' });
+    }
 
     res.status(200).json({ recipe });
   } catch (error) {
