9c69072ca862e81898519bb358c8e77acd74cb89
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-11-13 15:52:59 +0100

    controller to update a recipe created by a user

--- backend/controllers/recipe/updateRecipe.js
+++ backend/controllers/recipe/updateRecipe.js
@@ -3,16 +3,26 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export const updateRecipe = async (req, res) => {
+  const recipeID = req.params.recipeID;
+
   try {
     const recipe = await prisma.recipe.update({
       where: {
-        id: req.params.recipeID,
+        id: recipeID,
       },
       data: req.body,
       include: {
         ingredients: true,
       },
     });
+
+    if (!recipe) {
+      return res.status(404).json({ message: 'Recipe not found' });
+    }
+
+    if (recipe.userID !== req.body.userID) {
+      return res.status(403).json({ message: "You don't have permission to update this recipe" });
+    }
 
     res.json({ message: 'Recipe updated', recipe });
   } catch (error) {
