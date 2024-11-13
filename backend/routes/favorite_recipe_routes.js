b6b38c1a3446032dfa8b6677259bf0dad04c3241
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-11-13 11:04:46 +0100

    controller to get one specific recipe

--- backend/routes/favorite_recipe_routes.js
+++ backend/routes/favorite_recipe_routes.js
@@ -1,9 +1,15 @@
 import express from 'express';
 import { addToFavoriteRecipe } from '../controllers/favorite_recipe/addToFavoriteRecipe.js';
 import { deleteFromFavoriteRecipe } from '../controllers/favorite_recipe/deleteFromFavoriteRecipe.js';
+import {
+  getAllFavoriteRecipes,
+  getOneFavoriteRecipe,
+} from '../controllers/favorite_recipe/getFavoriteRecipes.js';
 
 const favoriteRecipeRouter = express.Router();
 
+favoriteRecipeRouter.get('/user/:userID/favorite/recipe', getAllFavoriteRecipes);
+favoriteRecipeRouter.get('/user/:userID/favorite/recipe/:recipeID', getOneFavoriteRecipe);
 favoriteRecipeRouter.post('/:userID/favorite/recipe/:recipeID', addToFavoriteRecipe);
 favoriteRecipeRouter.delete('/:userID/favorite/recipe/:recipeID', deleteFromFavoriteRecipe);
 
