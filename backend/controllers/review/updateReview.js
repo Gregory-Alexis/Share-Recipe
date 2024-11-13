f17924668b7f13a411dfa3fbbf58afaf149ee387
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-11-13 15:55:18 +0100

    controller to update a review & check if the user who's trying to delete the review his the one who created it.

--- backend/controllers/review/updateReview.js
+++ backend/controllers/review/updateReview.js
@@ -3,13 +3,23 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export const updateReview = async (req, res) => {
+  const reviewID = req.params.reviewID;
   try {
     const recipe = await prisma.review.update({
       where: {
-        id: req.params.reviewID,
+        id: reviewID,
       },
       data: req.body,
     });
+
+    if (!recipe) {
+      return res.status(404).json({ message: 'Review not found' });
+    }
+
+    if (recipe.userID !== req.body.userID) {
+      return res.status(403).json({ message: "You don't have permission to update this review" });
+    }
+
     res.status(200).json({ message: 'Review updated', recipe });
   } catch (error) {
     res.status(400).json({ message: error.message });
