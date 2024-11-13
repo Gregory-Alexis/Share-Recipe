8266698be929548c170eaff91bc52f2aae93ac96
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-11-13 10:32:13 +0100

    create controller to delete a recipe to the user's favorite

---
+++ backend/controllers/favorite_recipe/deleteFromFavoriteRecipe.js
@@ -0,0 +1,47 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export const deleteFromFavoriteRecipe = async (req, res) => {
+  try {
+    const userID = req.params.userID;
+    const recipeID = req.params.recipeID;
+
+    const user = await prisma.user.findUnique({
+      where: { id: userID },
+    });
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    const recipe = await prisma.recipe.findUnique({
+      where: { id: recipeID },
+    });
+
+    if (!recipe) {
+      return res.status(404).json({ message: 'Recipe not found' });
+    }
+
+    const existInFavorite = await prisma.favoriteRecipe.findFirst({
+      where: {
+        userID: userID,
+        recipeID: recipeID,
+      },
+    });
+
+    if (!existInFavorite) {
+      return res.status(400).json({ message: 'Recipe is not in your favorite' });
+    }
+
+    const deleteFromFavorite = await prisma.favoriteRecipe.delete({
+      where: {
+        id: existInFavorite.id,
+      },
+    });
+
+    res.status(201).json({ message: 'Recipe deleted from favorites', deleteFromFavorite });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
