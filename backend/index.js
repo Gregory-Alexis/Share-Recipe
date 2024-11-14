58636489ea4907d8bcc78dbed6be6f58addda59b
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-11-14 10:42:43 +0100

    Change secondary port if used & create a build if in production

--- backend/index.js
+++ backend/index.js
@@ -8,12 +8,20 @@ import router from './routes/index.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5173;
+const PORT = process.env.PORT || 5001;
 
 app.use(cors({ origin: `http://localhost:${PORT}`, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(router);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../frontend/dist', 'index.html'));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
