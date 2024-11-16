156d3e411089de7ca07244acefd3aa11ab5d4282
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-11-16 13:45:39 +0100

    return the component listing the recipes

---
+++ frontend/src/components/Recipes.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import RecipeListing from './RecipeCards';
+
+const Recipes: React.FC = () => {
+  return (
+    <>
+      <RecipeListing />
+    </>
+  );
+};
+export default Recipes;
