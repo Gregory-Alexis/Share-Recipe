e18261aa06f21146ee94e5ee6a9f4f4bfe152583
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-11-16 13:46:54 +0100

    utils to fetch all recipes &  recipe details

---
+++ frontend/src/utils/fetchData.ts
@@ -0,0 +1,29 @@
+import { RecipeModel } from './../models/Recipes';
+import axios from 'axios';
+
+const API_URL = 'http://localhost:5000/api';
+axios.defaults.withCredentials = true;
+
+export const fetchAllRecipes = async (): Promise<RecipeModel[]> => {
+  try {
+    const response = await axios.get(`${API_URL}/recipes`);
+    return response.data;
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error(`Error fetching recipes: ${error.message}`);
+    }
+  }
+  return [];
+};
+
+export const fetchOneRecipe = async (id: string): Promise<RecipeModel> => {
+  try {
+    const response = await axios.get(`${API_URL}/recipes/${id}`);
+    return response.data;
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error(`Error fetching recipe: ${error.message}`);
+    }
+  }
+  return {} as RecipeModel;
+};
