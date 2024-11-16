fd475876224b58bd5b79a347ed24872f7b40d684
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-11-16 13:41:54 +0100

    change the port to authorize the frond end

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-app.use(cors({ origin: `http://localhost:${PORT}`, credentials: true }));
+app.use(cors({ origin: `http://localhost:5173`, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(router);
