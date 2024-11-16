4293c5e120350d5cd47d42990f8f9bd76c69ef87
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-11-16 13:42:51 +0100

    app footer

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className='bg-orange-500 w-full py-6 text-white text-center'>
+      <p>&copy; 2023 ShareRecipe. Tous droits réservés.</p>
+      <p className='mt-2'>Suivez-nous sur les réseaux sociaux</p>
+    </footer>
+  );
+};
+export default Footer;
