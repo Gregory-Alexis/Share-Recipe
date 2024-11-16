c7f9c771e867a337063440bcaddb80d54b537ac7
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-11-16 13:42:43 +0100

    set up tanStack query

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,21 @@
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './router/router';
+
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <Navbar />
+        <RouterProvider router={router} />
+        <Footer />
+      </div>
+    </QueryClientProvider>
+  );
 };
 export default App;
