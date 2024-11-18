fc46c9d5e27e800eca127c4ff49af7fc1d97735a
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-11-18 14:57:55 +0100

    login page

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,4 +1,21 @@
-const LoginPage = () => {
+import React, { useState } from 'react';
+import { useRecipeStore } from '../store/recipeStore';
+
+const LoginPage: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { login, isLoading, error } = useRecipeStore();
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      await login(email, password);
+    } catch (err) {
+      console.error('Login error:', err);
+    }
+  };
+
   return (
     <div className='bg-orange-100 min-h-screen flex flex-col justify-center items-center'>
       <div className='text-center mb-8'>
@@ -8,7 +25,7 @@ const LoginPage = () => {
         </p>
       </div>
 
-      <form className='bg-white p-8 rounded-lg shadow-md w-full max-w-md'>
+      <form onSubmit={handleLogin} className='bg-white p-8 rounded-lg shadow-md w-full max-w-md'>
         <div className='mb-6'>
           <label htmlFor='email' className='block text-gray-700 font-semibold mb-2'>
             Email
@@ -19,9 +36,11 @@ const LoginPage = () => {
             name='email'
             placeholder='Entrez votre email'
             className='w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-orange-500'
-            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
+
         <div className='mb-6'>
           <label htmlFor='password' className='block text-gray-700 font-semibold mb-2'>
             Mot de passe
@@ -32,6 +51,8 @@ const LoginPage = () => {
             name='password'
             placeholder='Entrez votre mot de passe'
             className='w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:border-orange-500'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)} // Mise à jour de l'état password
             required
           />
         </div>
@@ -40,8 +61,10 @@ const LoginPage = () => {
           type='submit'
           className='w-full py-2 bg-orange-500 text-white font-semibold rounded-lg hover:bg-orange-600 transition'
         >
-          Se connecter
+          {isLoading ? 'Chargement...' : 'Se connecter'}
         </button>
+
+        {error && <p className='text-red-500 mt-4'>{error}</p>}
 
         <p className='mt-6 text-center text-gray-700'>
           Pas encore de compte ?{' '}
