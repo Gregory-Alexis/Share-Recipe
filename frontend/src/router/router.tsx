70abe3dd0f4b0cdf3bfb227bc744fd6f40465275
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-11-18 14:57:06 +0100

    app router

---
+++ frontend/src/router/router.tsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
+
+import HomePage from '../pages/HomePage';
+import LoginPage from '../pages/LoginPage';
+import SignUpPage from '../pages/SignUpPage';
+import RecipeDetails from '../components/RecipeDetails';
+import CreateRecipePage from '../pages/createRecipePage';
+
+export const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route>
+      <Route index element={<HomePage />} />
+      <Route path='/signup' element={<SignUpPage />} />
+      <Route path='/login' element={<LoginPage />} />
+      <Route path='/recipe/:id' element={<RecipeDetails />} />
+      <Route path='/create-recipe' element={<CreateRecipePage />} />
+      <Route path='*' element={''} />
+    </Route>
+  )
+);
