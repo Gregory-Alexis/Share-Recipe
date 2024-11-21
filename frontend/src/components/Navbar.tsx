5d836760cd7477f72c3036a7f79c6f5b5be376e9
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-11-21 17:38:35 +0100

    add the possibility to logout

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,14 +1,27 @@
+import { NavLink } from 'react-router-dom';
+import { useRecipeStore } from '../store/recipeStore';
+
 const Navbar = () => {
+  const { isAuthenticated, logout } = useRecipeStore();
+
+  const handleLogout = () => {
+    logout();
+  };
   return (
     <nav className='w-full bg-orange-500 text-white py-4 px-8 shadow-md flex justify-between items-center'>
       <h1 className='text-2xl font-bold'>Share recipe</h1>
       <div>
-        <button className='px-4 py-2 mr-4 bg-white text-orange-500 rounded-lg hover:bg-orange-600 hover:text-white transition'>
-          Se connecter
+        <button
+          onClick={handleLogout}
+          className='px-4 py-2 mr-4 bg-white text-orange-500 rounded-lg hover:bg-orange-600 hover:text-white transition'
+        >
+          <NavLink to='/login'>{isAuthenticated ? 'Se déconnecter' : 'Se connecter'}</NavLink>
         </button>
-        <button className='px-4 py-2 bg-white text-orange-500 rounded-lg hover:bg-orange-600 hover:text-white transition'>
-          S'inscrire
-        </button>
+        {!isAuthenticated && (
+          <button className='px-4 py-2 bg-white text-orange-500 rounded-lg hover:bg-orange-600 hover:text-white transition'>
+            <NavLink to='/signup'>S'inscrire</NavLink>
+          </button>
+        )}
       </div>
     </nav>
   );
