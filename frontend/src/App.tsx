575c76fc05c429a7dcf3b941d7bc5779a2c1e9d8
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-11-21 16:49:41 +0100

    remove component and user only the Router Provider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,3 @@
-import Footer from './components/Footer';
-import Navbar from './components/Navbar';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router/router';
 
@@ -10,11 +8,7 @@ const queryClient = new QueryClient();
 const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <div>
-        <Navbar />
-        <RouterProvider router={router} />
-        <Footer />
-      </div>
+      <RouterProvider router={router} />
     </QueryClientProvider>
   );
 };
