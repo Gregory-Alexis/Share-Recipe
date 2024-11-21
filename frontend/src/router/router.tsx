c299d6823bcdc4b2032206164688b7cd44926a07
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-11-21 17:30:49 +0100

    import Mainlayout component and wrap Sign up and Login page with the RedirectAuthenticatedUser component

--- frontend/src/router/router.tsx
+++ frontend/src/router/router.tsx
@@ -5,13 +5,29 @@ import LoginPage from '../pages/LoginPage';
 import SignUpPage from '../pages/SignUpPage';
 import RecipeDetails from '../components/RecipeDetails';
 import CreateRecipePage from '../pages/createRecipePage';
+import MainLayout from '../layout/MainLayout';
+import RedirectAuthenticatedUser from '../components/RedirectAuthenticatedUser';
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route>
+    <Route path='/' element={<MainLayout />}>
       <Route index element={<HomePage />} />
-      <Route path='/signup' element={<SignUpPage />} />
-      <Route path='/login' element={<LoginPage />} />
+      <Route
+        path='/signup'
+        element={
+          <RedirectAuthenticatedUser>
+            <SignUpPage />
+          </RedirectAuthenticatedUser>
+        }
+      />
+      <Route
+        path='/login'
+        element={
+          <RedirectAuthenticatedUser>
+            <LoginPage />
+          </RedirectAuthenticatedUser>
+        }
+      />
       <Route path='/recipe/:id' element={<RecipeDetails />} />
       <Route path='/create-recipe' element={<CreateRecipePage />} />
     </Route>
