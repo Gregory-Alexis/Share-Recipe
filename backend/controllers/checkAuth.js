0cf8982c468623d294e55ed179c504231854cb9d
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-11-21 17:23:54 +0100

    controller checking if a user is authenticated

---
+++ backend/controllers/checkAuth.js
@@ -0,0 +1,21 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+export const checkAuth = async (req, res) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: req.userID,
+      },
+    });
+
+    if (!user) {
+      return res.status(401).json({ message: 'User not found' });
+    }
+
+    res.status(200).json({ message: 'User authenticated' });
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
