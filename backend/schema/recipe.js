ff5b412d0c258ac0791508dbb73c7daeebf14512
Gregory-Alexis <gregoryalexiss@gmail.com>
2024-11-26 13:53:59 +0100

    remove ingredients parameter

--- backend/schema/recipe.js
+++ backend/schema/recipe.js
@@ -4,7 +4,6 @@ export const CreateRecipeSchema = z.object({
   title: z.string(),
   content: z.string(),
   image_url: z.string(),
-  ingredientIds: z.array(z.string()).optional(),
 });
 
 export const UpdateRecipeSchema = z.object({
